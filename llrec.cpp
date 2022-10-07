5e48251f7b3fc1c42a5cd7f7ac814637b88477d1
Morgan Okoreeh <okoreeh@usc.edu>
2022-10-07 02:32:35 +0000

    Initial commit for hw3

---
+++ llrec.cpp
@@ -0,0 +1,9 @@
+#include "llrec.h"
+
+//*********************************************
+// Provide your implementation of llpivot below
+//*********************************************
+
+void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot){
+		
+}
